9fcc270441e025d2bf5fd323870b85f805034651
Tyizo <69811071+tyizo@users.noreply.github.com>
2021-07-05 21:05:47 +0430

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,8 @@ client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.categories = fs.readdirSync("./commands/");
 client.prefix = config.prefix;
-const http = require("http");
-const express = require("express");
 const app = express();
 const moment = require("moment");
-
-// keep the app alive. it's useful when you upload this to glitch.com host !
-app.get("/", (request, response) => {
-  console.log(
-    `[${moment().format("DD-MM-YYYY HH:mm:ss")}]:` + " Ping Received!"
-  );
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-// handling the nodejs error.
-if (process.version.slice(1).split(".")[0] < 8)
-  throw new Error(
-    "Node 8.0.0 or higher is required. Update Node on your system."
-  );
-
 ["handlers", "events"].forEach((handler) => {
   require(`./handlers/${handler}`)(client);
 });
